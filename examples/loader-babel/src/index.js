e298cb62896f40183234ed379d1a70f5b93b401c
Fco Javier Núñez Berrocoso <javiernuber@gmail.com>
2018-10-07 19:02:18 +0200

    Example babel-loader

---
+++ examples/loader-babel/src/index.js
@@ -0,0 +1,4 @@
+import './styles.css';
+import { message } from './message-module.js'; 
+
+document.write(message);
---
+++ examples/loader-babel/webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const config = {
+    mode: 'development',
+    entry: path.resolve(__dirname, "src/index.js"),
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: 'main.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader"
+                ]            
+            },
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+          filename: "./css/styles.css"
+        })
+    ]
+}
+
+module.exports = config;
