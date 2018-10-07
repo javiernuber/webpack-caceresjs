2d4c8aa540263023613111b4642f6ae552d37070
Fco Javier Núñez Berrocoso <javiernuber@gmail.com>
2018-10-08 01:00:00 +0200

    Example loader-files

---
+++ examples/loader-files/src/js/message-module.js
@@ -0,0 +1,3 @@
+module.exports = {
+  message: "Logo JS Cáceres"
+}
---
+++ examples/loader-files/webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const config = {
+  mode: 'development',
+  entry: path.resolve(__dirname, "src/js/index.js"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: 'main.js',
+    publicPath: "./dist/"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: '../'
+            }
+          },
+          "css-loader"
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.(png|jpg|gif|woff|eot|ttf|svg)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 100000,
+            name: 'images/[name].[hash].[ext]',
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "./css/styles.css"
+    })
+  ]
+}
+
+module.exports = config;
